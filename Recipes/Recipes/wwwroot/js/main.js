2735b53bb3a82905ea46935dce87b3c78f2f51ce
Edina <edinakando@gmail.com>
2020-04-20 11:11:03 +0300

    -set loading overlay and style for confirm forms

---
+++ Recipes/Recipes/wwwroot/js/main.js
@@ -0,0 +1,25 @@
+﻿
+var timeout;
+$(document)
+    .ajaxStart(function () {
+        timeout = window.setTimeout(function () {
+            timeout = null;
+            $.LoadingOverlay("show", { maxSize: 40 });
+        }, 300);
+    })
+    .ajaxComplete(function () {
+        if (timeout) {
+            window.clearTimeout(timeout);
+            timeout = null;
+        }
+        $.LoadingOverlay("hide", { maxSize: 40 });
+    })
+
+
+jconfirm.defaults = {
+    useBootstrap: false,
+    type: 'orange',
+    boxWidth: '35%',
+    draggable: false,
+    escapeKey: 'cancel',
+}
